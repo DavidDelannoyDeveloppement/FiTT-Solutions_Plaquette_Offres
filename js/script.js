66aa0d74d431bd958b7f59a8ac62d8bb8cf4b2ec
{ D D D } <101110693+DavidDelannoyDeveloppement@users.noreply.github.com>
2023-07-24 15:19:26 +0200

    All files
    Maj après concertation du 7 juillet 2023

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//fonction apparition au scroll 1
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
     for (var i = 0; i < reveals.length; i++) {
@@ -15,6 +16,28 @@ function reveal() {
 window.addEventListener("scroll", reveal);
 
 
+//fonction apparition au scroll 2
+function reveal_2() {
+  var reveals = document.querySelectorAll(".reveal_2");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 50; //hauteur mini avant apparition
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active_2");
+    } else {
+      reveals[i].classList.remove("active_2");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal_2);
+
+
+
+
+
+
 
 //fonction page accueil
 window.addEventListener("load", loader);
@@ -23,3 +46,19 @@ function loader(){
   TLLOAD
   .to('.accueil', {height: 400, duration: 1.3, delay:0.4, ease: 'power2'})
 }
+
+
+
+//fonction appel use-case
+let hiders = document.querySelectorAll('[data-js="hide"]');
+
+Array.prototype.forEach.call(hiders, function (hider) {
+	let hiderID = hider.getAttribute('href');
+  let hiderTarget = document.querySelector(hiderID);
+  
+  hider.addEventListener('click', function (event) {
+  	event.preventDefault();
+    
+    hiderTarget.classList.toggle('-visible');
+  });
+});
